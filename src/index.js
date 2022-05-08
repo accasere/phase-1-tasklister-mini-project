46097bd514a0645748ed91772ea5e22660f61378
Antony Caseres <accasere@outlook.com>
2022-05-08 12:10:43 -0500

    Completed Assigment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.querySelector('#create-task-form').addEventListener('submit',(e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_task_description.value)
+  })
+})
+
+function buildToDo(todo) {
+  let listElement = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' X '
+  listElement.textContent = `${todo} `
+  listElement.appendChild(btn)
+  
+  document.querySelector('#tasks').appendChild(listElement)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+/*
+function buildToDo(todo){
+  let listElement = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.textContent = ' x '
+  listElement.textContent = `${todo} `
+  listElement.appendChild(btn)
+
+  document.querySelector('#list').appendChild(p)
+}
+
+*/
